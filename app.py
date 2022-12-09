6edf794b2232e018da4477b4b9421d8fbee18f78
Amzad hossain <amzad.rafi@northsouth.edu>
2022-12-09 22:29:33 +0600

    self mail slv . home page added flash msg bug sl

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-#import flask , render_template , request , redirect , url_for , session , flash,user login 
-from flask import Flask , render_template , request ,redirect,url_for,session,flash
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 from website import create_app 
+
 
 
 app = create_app() 
--- website/view.py
+++ website/view.py
@@ -1,12 +1,13 @@
-from flask import Flask ,Blueprint , render_template , request ,redirect,url_for,session,flash
-from flask_login import login_user,login_required,logout_user,current_user
+from flask import Blueprint , render_template , request ,redirect,url_for,flash
+from flask_login import login_required,current_user
 view = Blueprint('view',__name__) 
+from sqlalchemy import desc
 from .model import Inbox
 from . import db
 @view.route('/') 
 @login_required
 def home():
-    return 'this is not the right plce to be'
+    return render_template('home.html')
 
 @view.route('/dash',methods=['GET','POST']) 
 def user_dashboard():
@@ -16,22 +17,26 @@ def user_dashboard():
         subject = request.form.get('subject')
         massage = request.form.get('massage')   
         category = request.form.get('category')
-        new_inbox = Inbox(id=id,inbox=inbox,massage=massage,user_id=current_user.id,subject=subject,mail_form=current_user.email,category=category)
-        db.session.add(new_inbox)
-        db.session.commit()
-        flash('massage sent',category='success')
+      
+
+
+        if inbox==current_user.email: 
+            flash('you cant send a massage to yourself',category='error')
+            return redirect(url_for('view.user_dashboard'))
+        else: 
+            new_inbox = Inbox(id=id,inbox=inbox,massage=massage,user_id=current_user.id,subject=subject,mail_form=current_user.email,category=category)
+            db.session.add(new_inbox)
+            db.session.commit()
+            flash('massage sent',category='success')
         return redirect(url_for('view.user_dashboard'))
-    inbox = Inbox.query.filter_by(inbox=current_user.email).all()
-    #group by catagory and sort by date 
+  
     
 
 
 
-    find_all_primary = Inbox.query.filter_by(inbox=current_user.email,category='primary').all()
-    find_all_social = Inbox.query.filter_by(inbox=current_user.email,category='social').all()
-    find_all_promotion = Inbox.query.filter_by(inbox=current_user.email,category='promotion').all()
- 
+    find_all_primary = Inbox.query.filter_by(inbox=current_user.email,category='primary').order_by(desc(Inbox.date_created))
+    find_all_social = Inbox.query.filter_by(inbox=current_user.email,category='social').order_by(desc(Inbox.date_created))
+    find_all_promotion = Inbox.query.filter_by(inbox=current_user.email,category='promotion').order_by(desc(Inbox.date_created))
 
 
-
-    return render_template('user_dash.html',inbox=inbox, find_all_primary=find_all_primary,find_all_social=find_all_social,find_all_promotion=find_all_promotion)
+    return render_template('user_dash.html', find_all_primary=find_all_primary,find_all_social=find_all_social,find_all_promotion=find_all_promotion)
